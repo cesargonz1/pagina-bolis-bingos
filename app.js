9c008429c8e64124c9080ec50511186167e58c2b
cesargonz1 <cego2331@gmail.com>
2025-03-03 23:25:54 -0600

    cambiamos la pagina por la nueva

---
+++ app.js
@@ -0,0 +1,37 @@
+// Modo Oscuro
+const toggleDarkModeButton = document.getElementById("toggleDarkMode");
+const body = document.body;
+const sunIcon = '<i class="ri-sun-line"></i>';
+const moonIcon = '<i class="ri-moon-line"></i>';
+
+// Comprobar el estado guardado del modo oscuro en el localStorage
+if (localStorage.getItem("darkMode") === "enabled") {
+  body.classList.add("dark-mode");
+  toggleDarkModeButton.innerHTML = moonIcon;
+}
+
+toggleDarkModeButton.addEventListener("click", () => {
+  body.classList.toggle("dark-mode");
+
+  // Guardar el estado del modo oscuro en el localStorage
+  if (body.classList.contains("dark-mode")) {
+    localStorage.setItem("darkMode", "enabled");
+    toggleDarkModeButton.innerHTML = moonIcon;
+  } else {
+    localStorage.setItem("darkMode", "disabled");
+    toggleDarkModeButton.innerHTML = sunIcon;
+  }
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
